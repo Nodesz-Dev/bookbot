99ba7bfa33e652895210264c61e7c702bcc3465c
Nodesz-Dev <harleykempita@gmail.com>
2025-05-24 11:14:58 +1000

    Added get book text function

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def get_book_text (book_file_path):
+    Book_text = "";
+    with open(book_file_path) as f:
+        Book_text = f.read();
+    return Book_text;
+
+def main ():
+    print(get_book_text("./books/frankenstein.txt"))
+
+main()
