3190cfbbfe12ca46e7c8076587391b8bbb003921
Nodesz-Dev <harleykempita@gmail.com>
2025-05-26 17:39:58 +1000

    Refactored - stats.py created and now houses the count words function - added pycache to .gitignore

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
+from stats import count_words
+
 def get_book_text (book_file_path):
     Book_text = "";
     with open(book_file_path) as f:
         Book_text = f.read();
     return Book_text;
-
-def count_words(text_to_count):
-    list_of_words = text_to_count.split()
-    return len(list_of_words);
 
 def main ():
     #print(get_book_text("./books/frankenstein.txt"))
---
+++ stats.py
@@ -0,0 +1,3 @@
+def count_words(text_to_count):
+    list_of_words = text_to_count.split()
+    return len(list_of_words);
